76da1d28f916d5c0591d84bd29dabfda5d60273a
rayen louati <rayen@192.168.100.43>
2025-05-20 23:37:55 +0100

    Initial commit with React portfolio and floating navbar

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
 
   return (
     <>
-      <Navbar />
+        <Navbar />
 
       <div id="Home" className="container2">
       <div className="container2">
@@ -141,6 +141,7 @@ Methodology
       {/* External Bootstrap (only if needed) */}
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
     </>
+    
   );
 }
 
