5fe3c5cb6d158f0e1235942f016e76d309249f8e
Rayen Louati <152817290+rayen548@users.noreply.github.com>
2025-05-20 23:37:06 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,29 @@
 import React from 'react';
+import { FaHome, FaUser, FaTools, FaProjectDiagram, FaEnvelope } from 'react-icons/fa';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
-      <div className="container">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a href="#Home" className="nav-link">Home</a>
-            </li>
-            <li className="nav-item">
-              <a href="#About" className="nav-link">About</a>
-            </li>
-            <li className="nav-item">
-              <a href="#Skills" className="nav-link">Skills</a>
-            </li>
-            <li className="nav-item">
-              <a href="#Projects" className="nav-link">Projects</a>
-            </li>
-            <li className="nav-item">
-              <a href="#Contact" className="nav-link">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <nav className="floating-navbar">
+      <a href="#Home">
+        <FaHome />
+        <div>Home</div>
+      </a>
+      <a href="#About">
+        <FaUser />
+        <div>About</div>
+      </a>
+      <a href="#Skills">
+        <FaTools />
+        <div>Skills</div>
+      </a>
+      <a href="#Projects">
+        <FaProjectDiagram />
+        <div>Projects</div>
+      </a>
+      <a href="#Contact">
+        <FaEnvelope />
+        <div>Contact</div>
+      </a>
     </nav>
   );
 }
